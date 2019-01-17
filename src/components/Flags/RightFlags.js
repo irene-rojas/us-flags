130ba6f1b8a6392609b4d132e29a7958aa11b72c
Irene R <irene@smallwondermedia.com>
2019-01-17 17:48:13 -0500

    start rightFlags

--- src/components/Flags/RightFlags.js
+++ src/components/Flags/RightFlags.js
@@ -1,3 +1,25 @@
 import React from "react";
+import "./Flags.css";
+import montana from "./images/Montana.png";
+import nebraska from "./images/Nebraska.png";
+import nevada from "./images/Nevada.png";
+import newHamp from "./images/NewHampshire.png";
+import newJersey from "./images/NewJersey.png";
+import newMex from "./images/NewMexico.png";
+import newYork from "./images/NewYork.png";
+import noCaro from "./images/NorthCarolina.png";
+import noDak from "./images/NorthDakota.png";
+import ohio from "./images/Ohio.png";
+import okla from "./images/Oklahoma.png";
+import oregon from "./images/Oregon.png";
+
+const RightFlags = () => (
+
+    <div className="rightFlagDiv">
+
+    </div>
+
+
+)
 
 export default RightFlags;
