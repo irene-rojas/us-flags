d90a33be076da77e40d73b54b2bad8674147611f
Irene R <irene@smallwondermedia.com>
2019-01-20 15:14:41 -0500

    returned other components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Map from "./components/Map/Map.js";
-// import TopFlags from "./components/Flags/TopFlags";
-// import LeftFlags from "./components/Flags/LeftFlags";
-// import RightFlags from "./components/Flags/RightFlags";
-// import BottomFlags from "./components/Flags/BottomFlags";
+import Map from "./components/Map/Map.js";
+import TopFlags from "./components/Flags/TopFlags";
+import LeftFlags from "./components/Flags/LeftFlags";
+import RightFlags from "./components/Flags/RightFlags";
+import BottomFlags from "./components/Flags/BottomFlags";
 import AppDragDropDemo from './AppDragDropDemo';
 
 class App extends Component {
@@ -19,23 +19,23 @@ class App extends Component {
       <div className="App">
 
         <div className="topDiv">
-            {/* <TopFlags draggable="true" /> */}
+            <TopFlags draggable="true" />
         </div>
 
         <div className="leftDiv">
-            {/* <LeftFlags draggable="true"/> */}
+            <LeftFlags draggable="true"/>
         </div>
 
         <div className="rightDiv">
-            {/* <RightFlags draggable="true"/> */}
+            <RightFlags draggable="true"/>
         </div>
 
         <div className="bottomDiv">
-            {/* <BottomFlags draggable="true"/> */}
+            <BottomFlags draggable="true"/>
         </div>
 
         <div className="mapDiv">
-            {/* <Map /> */}
+            <Map />
             {/* 0.6183 */}
         </div>
 
